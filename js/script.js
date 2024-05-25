d6ff19e726d7e957e2c4bab6713addfa10c18dd6
envr7 <146791047+envr7@users.noreply.github.com>
2024-05-25 21:13:38 +0700

    js

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let userBox = document.querySelector('.header .header-2 .user-box');
+
+document.querySelector('#user-buton').onclick = () =>{
+   userBox.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .header-2 .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   userBox.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   userBox.classList.remove('active');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .header-2').classList.add('active');
+   }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
+
+var swiper = new Swiper(".slide-content", {
+   slidesPerView: 3,
+   spaceBetween: 25,
+   loop: true,
+   centerSlide: 'true',
+   fade: 'true',
+   grabCursor: 'true',
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+
+   breakpoints:{
+       0: {
+           slidesPerView: 1,
+       },
+       520: {
+           slidesPerView: 2,
+       },
+       950: {
+           slidesPerView: 3,
+       },
+   },
+ });
